e41f85d0bd9013c6f83cd7f065bdc215f749b56a
matthiasliszt <mliszt.program@gmail.com>
2018-06-26 11:42:29 +0200

    bootstrapped

--- intermediate/intermediate.js
+++ intermediate/intermediate.js
@@ -14,12 +14,23 @@ function replaceAll(data,search, replacement) {
 }
 
 function showSongs(data){
- var v1=replaceAll(data,"<song>","<div class='col-lg-6 col-md-6'>");
- var v2=replaceAll(v1,"</song>","</div>");
+ var v1=replaceAll(data,"<song>","<div class='col-lg-4 col-md-4 col-sm-12 song'><div>");
+ var v2=replaceAll(v1,"</song>","</div></div>");
  var v3=replaceAll(v2,"</songtitle>","</songtitle><br/>");
- $("#songs").html(v3);
+ var v4=replaceAll(v3,"</artist>","</p>");
+ var v5=replaceAll(v4,"</genre>","</p>");
+ var v6=replaceAll(v5,"</country>","</p>");
+ var v7=replaceAll(v6,"</year>","</p>");
+ var v8=replaceAll(v7,"<artist>","<p>");
+ var v9=replaceAll(v8,"<genre>","<p>");
+ var v10=replaceAll(v9,"<country>","<p>");
+ var v11=replaceAll(v10,"<year>","<p>");
+
+
+ $("#songs").html(v11);
  console.log('data should be there');
 }
+
 
 console.log('you alive :)');
 loadSongs("song.xml");
